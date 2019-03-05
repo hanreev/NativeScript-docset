cb71c30daf6e50222c1da80dcb5665823ac68fb1
Rifa'i M. Hanif <hanreev@gmail.com>
2019-03-05 22:10:59 +0700

    initial

---
+++ generate-ns-docset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export TYPEDOC_DASH_ICONS_PATH=./ns-dash-icons
+
+rm -rf NativeScript.docset
+
+if [[ ! -d typedoc-dash-theme && ! -d NativeScript ]]; then
+  git submodule update --init
+fi
+
+if [ ! -d typedoc-dash-theme/node_modules ]; then
+  cd typedoc-dash-theme
+  yarn install
+  cd ..
+fi
+
+cd NativeScript
+if [ ! -d "node_modules" ]; then
+  npm i
+fi
+
+node_modules/.bin/typedoc --tsconfig tsconfig.typedoc.json \
+  --name NativeScript \
+  --out ../NativeScript.docset \
+  --theme ../typedoc-dash-theme/bin \
+  --includeDeclarations \
+  --excludeExternals \
+  --externalPattern "**/+(tns-core-modules|module).d.ts"
